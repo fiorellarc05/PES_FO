b6db1ffb2c497b94462b5cd62ccf3c1ef3674bbb
Fiorella RC <fiorellarodriguez05.fr@gmail.com>
2021-07-17 14:27:27 -0600

    log in

--- PES/src/app/app.module.ts
+++ PES/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHomeComponent } from './main-home/main-home.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHomeComponent
+    MainHomeComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
